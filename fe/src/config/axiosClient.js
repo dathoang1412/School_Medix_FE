4a01a415390d5bc5ecaa0df1ee8aa51caca3ffc1
Dat Hoang Tan <jezzthedead@gmail.com>
2025-06-11 23:24:08 +0700

    config axios

---
+++ fe/src/config/axiosClient.js
@@ -0,0 +1,32 @@
+// src/api/axiosClient.js
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000, 
+});
+
+// axiosClient.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token'); // hoặc từ Redux, Zustand...
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// // Response Interceptor (handle lỗi chung)
+// axiosClient.interceptors.response.use(
+//   (response) => response.data,
+//   (error) => {
+//     console.error('API error:', error.response || error.message);
+//     return Promise.reject(error);
+//   }
+// );
+
+export default axiosClient;
