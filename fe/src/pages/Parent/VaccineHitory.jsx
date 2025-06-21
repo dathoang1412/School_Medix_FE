e3d64775d96ba6fd36c5d11eddc685cdcec40c2d
Dat Hoang Tan <jezzthedead@gmail.com>
2025-06-21 10:47:22 +0700

    backup

--- fe/src/pages/Parent/VaccineHitory.jsx
+++ fe/src/pages/Parent/VaccineHitory.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import axiosClient from '../../config/axiosClient';
 
 const VaccineHitory = () => {
 
   const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    
+    const fecthHistory = async () => {
+      const res = axiosClient.get("")
+    }
   }, [])
 
   return (
